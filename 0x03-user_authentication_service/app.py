933eceef533d5ca1df42518029180537907c0160
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2024-02-21 10:38:32 +0300

    feat: no 7

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,8 +2,11 @@
 """
 File for the flask APP
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 app = Flask(__name__)
+
+AUTH = Auth()
 
 
 @app.route("/", methods=["GET"], strict_slashes=False)
@@ -12,6 +15,19 @@ def index():
     Simple get route
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def users():
+    """
+    Endpoint for registering users
+    """
+    data = request.form
+    try:
+        AUTH.register_user(data['email'], data['password'])
+        return jsonify({"email": data["email"], "message": "user created"})
+    except Exception as e:
+        return jsonify({"message": "email already registered"})
 
 
 if __name__ == "__main__":
