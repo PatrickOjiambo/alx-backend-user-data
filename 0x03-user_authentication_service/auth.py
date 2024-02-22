ec41a10a58b21cb9d9246aa0f617492d5d549096
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2024-02-22 12:08:46 +0300

    feat: no 13

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -80,3 +80,9 @@ class Auth:
             return self._db.find_user_by(session_id=session_id)
         except Exception as e:
             return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """
+        Destroy user session
+        """
+        return self._db.update_user(user_id=user_id, session_id=None)
