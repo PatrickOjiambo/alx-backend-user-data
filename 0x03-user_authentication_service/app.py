e15ee6257f6cd84c73ee7b4dd5c8dfc0e9361d64
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2024-02-22 11:47:28 +0300

    feat: no 11

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """
 File for the flask APP
 """
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, make_response, abort, Response
 from auth import Auth
 app = Flask(__name__)
 
@@ -28,6 +28,28 @@ def users():
         return jsonify({"email": data["email"], "message": "user created"})
     except Exception as e:
         return jsonify({"message": "email already registered"})
+
+
+@app.route("/sessions", methods=["POST"], strict_slashes=False)
+def login():
+    """
+    Route for login to the app
+    """
+    data = request.form
+    try:
+        is_logged_in = AUTH.valid_login(data['email'], data['password'])
+        if is_logged_in is True:
+            session_id = AUTH.create_session(data["email"])
+            resp = make_response(
+                jsonify({"email": data["email"], "message": "logged in"}))
+            print(resp)
+            resp.set_cookie("session_id", session_id)
+            print(resp)
+            return resp
+        elif is_logged_in is False:
+            abort(401)
+    except Exception as e:
+        abort(401)
 
 
 if __name__ == "__main__":
