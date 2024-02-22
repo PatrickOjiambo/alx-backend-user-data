7852f91009d01f2d0b5d28e928c50ac4a1538713
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2024-02-22 12:28:24 +0300

    fix: 403 response

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -58,7 +58,7 @@ def logout():
     session_id = request.cookies.get('session_id')
     user = AUTH.get_user_from_session_id(session_id)
     if user is not None:
-        AUTH.destroy_session(user.session_id)
+        AUTH.destroy_session(user.id)
         return redirect("/")
     else:
         abort(403)
